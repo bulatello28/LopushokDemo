c5ebcb772c0b60c8e373ea73623eacf9cd1d1ee3
Shaykhut <besit228@inbox.lv>
2024-02-26 17:17:36 +0300

    Создание ADO модели и подключения к нему

--- WPFAppForDemo/WPFAppForDemo/App.xaml.cs
+++ WPFAppForDemo/WPFAppForDemo/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using WPFAppForDemo.ADO;
 
 namespace WPFAppForDemo
 {
@@ -13,5 +14,6 @@ namespace WPFAppForDemo
     /// </summary>
     public partial class App : Application
     {
+        public static DemoPracticeShaykhutdinov420Entities Connection = new DemoPracticeShaykhutdinov420Entities();
     }
 }
